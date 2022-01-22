05a5e1188f7f1ef4f36df654b6af3287f062a7ce
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-23 02:22:16 +0300

    let

--- app/src/main/java/com/metehanbolat/kotlinscopefunctions/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/kotlinscopefunctions/MainActivity.kt
@@ -8,11 +8,28 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
+    private var number: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        // Let is usually used for null check.
+
+        // Even if we check for null with "if", a different kernel may access the variable and cause the application to crash.
+        // Also, even if we checked for null, the variable is still nullable.
+        if (number != null) {
+            val number2 = number + 1
+        }
+        // That's why we have to use "Let"
+        // We can also declare the let block as a value.
+        // The last line of the let block will act as a return.
+        // Also we can use elvis operator with let block.
+        val x = number?.let {
+            val number2 = it + 1
+            number2
+        } ?: 3
     }
 }
